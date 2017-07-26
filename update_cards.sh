ce443572bf81dc5861cfedd594db3190bbde4dfe
Nick Fraker <nick.fraker@ijettechnologies.com>
2017-07-26 15:04:11 -0700

    code comments for update_card

--- update_cards.sh
+++ update_cards.sh
@@ -1,26 +1,38 @@
 #!/bin/bash -e
 
+### Setting default values.
+# URL - the target to query for scraping cards out of
 url="http://192.168.59.104/card"
+# Stop - what message to search for that would signify we've asked for a non-existant card
 stop="Whoops! You found an error."
+# Sets - What sets (Opuses) of cards we want to retrieve.
 sets=("1" "2" "3" "pr")
+###
 
+# Housekeeping
 if [[ -f 'cards.sql' ]]; then
   rm 'cards.sql'
 fi
 
+# Purge all old Card table entries, thus replacing them with our new ones. Comment these lines out if you're looking to append, not replace
 echo "delete from cards;" > cards.sql
 echo "ALTER TABLE cards AUTO_INCREMENT = 1" >> cards.sql
 
+# Header for our imminent dump of card data to stdout.
 printf '%-30s%-20s%-20s%-20s%-30s%-20s%-20s%-20s%-20s\n' "NAME" "ELEMENT" "COST" "TYPE" "JOB" "CATEGORY" "POWER" "RARITY" "NUMBER"
+
 for s in "${sets[@]}"; do
+  # More housekeeping
   if [[ -f '.cache' ]]; then
     rm '.cache'
   fi
 
   touch .cache
   i="1"
+  # "So long as we haven't seen the 'stop' trigger..."
   while [[ "$(grep "$stop" .cache)" == "" ]]; do
     curl "$url/$s-$i" 2>/dev/null > .cache
+    # Display what cards we've found, and the information we parsed for each card. Allows for review prior to implementation.
     card_name="$(grep "<h4>" .cache | sed -e 's/.*<h4>//g' -e "s/&#039;/'/g")"
     card_elem="$(grep ".png\" /> (" .cache | sed -e 's/.*(//g' -e 's/).*//g')"
     card_type="$(grep -A 1 ">Type<" .cache | grep -v ">Type<" | sed -e 's/.*<td>//g' -e 's/<\/td>.*//g')"
@@ -33,14 +45,18 @@ for s in "${sets[@]}"; do
     card_text="$(grep -A 1 ">Card Text<" .cache | grep -v ">Card Text<" | sed -e 's/.*<td><p>//g' -e 's/<\/p><\/td>.*//g' -e "s/&#039;/'/g")"
     printf '%-30s%-20s%-20s%-20s%-30s%-20s%-20s%-20s%-20s\n' "$card_name" "$card_elem" "$card_cost" "$card_type" "$card_job" "$card_cat" "$card_pwr" "$card_rare" "$card_num"
 
+    # The database wants an integer for card_power, so ensure it gets one.
     if [[ "$card_cost" == "" ]]; then
       card_cost="0"
     fi
 
+    # Make sure we don't mistake an error page for a card when making database entries
     if [[ "$(grep "$stop" .cache)" == "" ]]; then
       if echo "$card_type" | grep -qi "forward"; then
+        # This is a conditional insert - it will only insert the card if one with the same set number and card number is not found in the database.
         printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, power, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$i" "$card_rare" "$card_pwr" "$i" "$s" >> cards.sql
       else
+        # If the card we retrieved is not a forward, omit the 'power' value. Otherwise SQL will complain we aren't providing an integer.
         printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$i" "$card_rare" "$i" "$s" >> cards.sql
       fi
     fi
@@ -49,6 +65,7 @@ for s in "${sets[@]}"; do
   done
 done
 
+# more housekeeping
 if [[ -f '.cache' ]]; then
   rm '.cache'
 fi
