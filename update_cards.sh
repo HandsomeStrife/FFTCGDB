f67cbb31b4733d71c9f9e7ec893ed31412bdaee0
Nick Fraker <nick.fraker@ijettechnologies.com>
2017-07-26 22:36:41 -0700

    Tweaked the script to generate a sync file, not a new table file

--- update_cards.sh
+++ update_cards.sh
@@ -15,8 +15,8 @@ if [[ -f 'cards.sql' ]]; then
 fi
 
 # Purge all old Card table entries, thus replacing them with our new ones. Comment these lines out if you're looking to append, not replace
-echo "delete from cards;" > cards.sql
-echo "ALTER TABLE cards AUTO_INCREMENT = 1" >> cards.sql
+#echo "delete from cards;" > cards.sql
+#echo "ALTER TABLE cards AUTO_INCREMENT = 1" >> cards.sql
 
 # Header for our imminent dump of card data to stdout.
 printf '%-30s%-20s%-20s%-20s%-30s%-20s%-20s%-20s%-20s\n' "NAME" "ELEMENT" "COST" "TYPE" "JOB" "CATEGORY" "POWER" "RARITY" "NUMBER"
@@ -69,10 +69,12 @@ for s in "${sets[@]}"; do
     if [[ "$(grep "$stop" .cache)" == "" ]]; then
       if echo "$card_type" | grep -qi "forward"; then
         # This is a conditional insert - it will only insert the card if one with the same set number and card number is not found in the database.
-        printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, power, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$(printf "%03d", "$i")" "$card_rare" "$card_pwr" "$(printf "%03d", "$i")" "$s" >> cards.sql
+#        printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, power, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$(printf "%03d", "$i")" "$card_rare" "$card_pwr" "$(printf "%03d", "$i")" "$s" >> cards.sql
+         printf "UPDATE cards SET name = '%s', cost = '%s', element = '%s', type = '%s', job = '%s', category = '%s', text = '%s', rarity = '%s', power ='%s', updated_at = NOW() where set_number = '%s' and TRIM(LEADING '0' FROM card_number) = '%s' limit 1;\n" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$card_rare" "$card_pwr" "$s" "$i" >> cards.sql
       else
         # If the card we retrieved is not a forward, omit the 'power' value. Otherwise SQL will complain we aren't providing an integer.
-        printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$(printf "%03d", "$i")" "$card_rare" "$(printf "%03d", "$i")" "$s" >> cards.sql
+#        printf "INSERT INTO cards ( set_number, name, cost, element, type, job, category, text, card_number, rarity, created_at, updated_at ) select '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', NOW(), NOW() from DUAL where not exists ( select id from cards where card_number = '%s' and set_number = '%s' limit 1 );\n" "$s" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$(printf "%03d", "$i")" "$card_rare" "$(printf "%03d", "$i")" "$s" >> cards.sql
+         printf "UPDATE cards SET name = '%s', cost = '%s', element = '%s', type = '%s', job = '%s', category = '%s', text = '%s', rarity = '%s', updated_at = NOW() where set_number = '%s' and TRIM(LEADING '0' FROM card_number) = '%s' limit 1;\n" "$(echo $card_name | sed -e "s/'/\\\'/g")" "$card_cost" "$(echo "$card_elem" | awk '{print tolower($0)}')" "$(echo "$card_type" | awk '{print tolower($0)}')" "$(echo $card_job | sed -e "s/'/\\\'/g")" "$card_cat" "$(echo $card_text | sed -e "s/'/\\\'/g")" "$card_rare" "$s" "$i" >> cards.sql
       fi
     fi
 
